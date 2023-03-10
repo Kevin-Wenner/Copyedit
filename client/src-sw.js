e68ed3869d36bec7308fd9eb10185a091127d3da
unknown <kevwenner@gmail.com>
2023-03-09 21:07:30 -0500

    fixed spelling mistake

--- client/src-sw.js
+++ client/src-sw.js
@@ -29,7 +29,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache),
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ['image'," script", "style"].includes(requres.destination),
+  ({ request }) => ['image'," script", "style"].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: 'my-cache',
     plugins: [
