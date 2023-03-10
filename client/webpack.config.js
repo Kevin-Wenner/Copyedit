c72d2f0fc2abd07a20dca48313c226a1f9ac47a9
unknown <kevwenner@gmail.com>
2023-03-09 20:45:20 -0500

    change to route

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -24,7 +23,10 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: 'src-sw.js'
       }),
-      new WebpackPwaManifest({name: 'copyedit',
+      new WebpackPwaManifest({
+      fingerprints: "false",
+      name: 'copyedit',
+      inject: true,
       short_name: 'J.A.T.E',
       description: 'JATE',
       background_color: '#7eb4e2',
